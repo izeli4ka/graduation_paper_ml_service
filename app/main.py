d826a8a292c451ef97a57ab57d63d0c932c281e1
izeli4ka <108812531+izeli4ka@users.noreply.github.com>
2025-05-12 18:01:39 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,24 +1,25 @@
+import os
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.api.routes import router
 
-
 app = FastAPI(title="Scientific Poster ML Service")
 
-
 # Настройка CORS
+allowed_origins = os.getenv(
+    "ALLOWED_ORIGINS",
+    "http://localhost:8080,http://localhost:8000"
+).split(",")
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:8080", "http://localhost:8000"],  # Фронтенд и бэкенд
+    allow_origins=allowed_origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-
 # Подключение роутов
 app.include_router(router, prefix="/api/ml", tags=["ml"])
-
 
 @app.get("/")
 def read_root():
